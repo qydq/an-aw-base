672faf9ad4df7319a65044b2a62fbafedb636d29
shiluohua <staryumou@163.com>
2017-04-24 14:01:04 +0800

    takephoto整合。

---
+++ an-base/src/main/java/com/an/base/utils/ytips/InvokeListener.java
@@ -0,0 +1,10 @@
+package com.an.base.utils.ytips;
+
+import com.jph.takephoto.model.InvokeParam;
+
+/**
+ * 授权管理回调
+ */
+public interface InvokeListener {
+    PermissionManager.TPermissionType invoke(InvokeParam invokeParam);
+}
